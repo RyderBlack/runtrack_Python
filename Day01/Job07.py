e41e57ab204e2dde85fdcf77fa143892d5af0dba
rydeblack <ryhad.boughanmi@gmail.com>
2025-02-03 14:09:25 +0100

    job 7 done

---
+++ Day01/Job07.py
@@ -0,0 +1,25 @@
+class Character():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def move_up(self):
+        self.y += 1
+        return self.y
+    
+    def move_down(self):
+        self.y -= 1
+        return self.y
+    
+    def move_right(self):
+        self.x += 1
+        return self.x
+    
+    def move_left(self):
+        self.x -= 1
+        return self.x
+    
+char01 = Character(22,33)
+
+print(char01.move_down())
+print(char01.move_right())
