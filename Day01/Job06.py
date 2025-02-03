33e1e512a6249e3db1d7f7d94869dbb555255351
rydeblack <ryhad.boughanmi@gmail.com>
2025-02-03 13:49:12 +0100

    job 6 done

---
+++ Day01/Job06.py
@@ -0,0 +1,18 @@
+class Animal():
+    def __init__(self):
+        self.age = 0
+        self.name = ""
+        
+    def aging(self):
+        self.age += 1
+        return f"Your animal's age is now {self.age} year old"
+    
+    def naming(self):
+        self.name = input("Name your pet here: ")
+        return f"Your pet's name is now {self.name}!"
+        
+tiger = Animal()
+print(f"Your animal's age is : {tiger.age}")
+print(tiger.aging())
+
+print(tiger.naming())
