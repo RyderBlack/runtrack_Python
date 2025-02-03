41969b0ce0cfa6e69d18e84054be29ac60d13778
rydeblack <ryhad.boughanmi@gmail.com>
2025-02-03 12:15:45 +0100

    job 2 done

---
+++ Day01/Job02.py
@@ -0,0 +1,8 @@
+class Operations():
+    def __init__(self):
+        self.number01 = 12
+        self.number02 = 3
+        
+operations = Operations()
+print(f"Le nombre1 est {operations.number01}")
+print(f"Le nombre2 est {operations.number02}")
